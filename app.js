9f18c6bb880f9095c7ccfffe928f23f0528a3dd8
billy adamowski <billy.adamowski@gmail.com>
2014-06-10 18:24:51 -0500

    Basic Start of application

---
+++ app.js
@@ -0,0 +1,27 @@
+var express = require('express'),
+  nodemailer = require("nodemailer");
+
+var app = express();
+
+// configure Express
+app.configure(function() {
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'ejs');
+  app.engine('ejs', require('ejs-locals'));
+  app.use(express.cookieParser());
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(express.session({ secret: 'ash ketchum' }));
+  app.use(app.router);
+  app.use(express.static(__dirname + '/public'));
+  app.use(express.logger());
+});
+
+
+app.get('/', function(req, res){
+  res.render('index', {});
+});
+
+app.listen(8080, function() {
+  console.log("Application started on port 8080!");
+});
