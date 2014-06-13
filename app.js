12b432f135caa318d2498391f3b225a30a7c1b63
Eric Ponto <eric.ponto@gmail.com>
2014-06-12 20:26:13 -0500

    ionic seed project commit

--- app.js
+++ app.js
@@ -13,14 +13,14 @@ app.configure(function() {
   app.use(express.methodOverride());
   app.use(express.session({ secret: 'ash ketchum' }));
   app.use(app.router);
-  app.use(express.static(__dirname + '/public'));
+  app.use(express.static(__dirname + '/chrysalis-public/www'));
   app.use(express.logger());
 });
 
 
-app.get('/', function(req, res){
+/*app.get('/', function(req, res){
   res.render('index', {});
-});
+});*/
 
 app.listen(8080, function() {
   console.log("Application started on port 8080!");
