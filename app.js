d8119c6cf5ae17b69190adf5b6f8167b9bd777eb
billy adamowski <billy.adamowski@gmail.com>
2014-06-13 10:55:53 -0500

    Basic Security

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 var express = require('express'),
-  nodemailer = require("nodemailer");
+  nodemailer = require("nodemailer"),
+  passport = require('passport'), 
+  LocalStrategy = require('passport-local').Strategy,
+  mongoose = require("mongoose");
 
 var format = require('util').format;
 
@@ -7,12 +10,34 @@ var app = express();
 var helloName = require("./server/helloName");
 var api = require("./server/api");
 
-var mongoose = require("mongoose");
-
 mongoose.connect("mongodb://localhost/chrysalis");
 var db = mongoose.connection;
 
 var KittenModel = require("./server/model/Kitten")(mongoose);
+var AdminModel = require("./server/model/Admin")(mongoose);
+
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
+passport.deserializeUser(function(id, done) {
+  AdminModel.findById(id,function(err,user){
+    done(err,user);
+  });
+});
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+    AdminModel.findOne({ username: username }, function(err, user) {
+      if (err) { return done(err); }
+      if (!user) {
+        return done(null, false, { message: 'Incorrect username.' });
+      }
+      if (user.password != password) {
+        return done(null, false, { message: 'Incorrect password.' });
+      }
+      return done(null, user);
+    });
+  }
+));
 
 // configure Express
 app.configure(function() {
@@ -23,9 +48,32 @@ app.configure(function() {
   app.use(express.bodyParser());
   app.use(express.methodOverride());
   app.use(express.session({ secret: 'ash ketchum' }));
+  app.use(passport.initialize());
+  app.use(passport.session());
   app.use(app.router);
   app.use(express.static(__dirname + '/chrysalis-public/www'));
   app.use(express.logger());
+});
+
+app.post('/admin/login', function(req, res, next) {
+  passport.authenticate('local', function(err, user, info) {
+    if (err) { return next(err) }
+    if (!user) {
+      req.session.messages =  [info.message];
+      return res.redirect('/login')
+    }
+    req.logIn(user, function(err) {
+      if (err) { return next(err); }
+      return res.redirect('/');
+    });
+  })(req, res, next);
+});
+
+app.get('/unsecure', function(req, res){
+  return res.send("TEST");
+});
+app.get('/secure', ensureAuthenticated, function(req, res){
+  return res.send("SECURE");
 });
 
 app.get("/kittens", function(req, res){
@@ -55,3 +103,8 @@ app.post("/hello", helloName);
 app.listen(8080, function() {
   console.log("Application started on port 8080!");
 });
+
+function ensureAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) { return next(); }
+  res.redirect('/login')
+}
