4f573b410d1a943b0dd117a8262efef28907d154
David Murley <david.murley@drake.edu>
2014-06-13 11:47:32 -0500

    Moved endpoints outside of app.js

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express'),
   nodemailer = require("nodemailer");
 
+
 var format = require('util').format;
 
 var app = express();
@@ -14,6 +15,7 @@ mongoose.connect("mongodb://localhost/chrysalis");
 var db = mongoose.connection;
 
 var models = require("./server/model/models")(mongoose);
+var endpoints = require("./server/endpoints")(models);
 
 // configure Express
 app.configure(function() {
@@ -28,107 +30,23 @@ app.configure(function() {
   app.use(express.static(__dirname + '/chrysalis-public/www'));
   app.use(express.logger());
 });
-app.get("/message", function(req, res){
-  return models.message.find("", function(err, data){
-    if (!err){ 
-      return res.send(data)
-    }
-    else{
-       return console.error(err);
-    }
 
-  }); 
-});
-
-app.get('/message/:id', function (req, res){
-  return models.message.findById(req.params.id, function (err, data) {
-    if (!err) {
-      return res.send(data);
-    } else {
-      return console.error(err);
-    }
-  });
-});
-
-app.post("/message", function(req, res){
-  var newTag = new models.message(req.body);
-  newTag.save(function (err) {
-    if (err) {
-      return console.log(err);
-    }
-  });
-  return res.send(newTag);
-});
-
-
-app.get("/image", function(req, res){
-  return models.image.find("", function(err, data){
-    if (!err){ 
-      return res.send(data)
-    }
-    else{
-       return console.error(err);
-    }
-
-  }); 
-});
-
-app.get('/image/:id', function (req, res){
-  return models.image.findById(req.params.id, function (err, data) {
-    if (!err) {
-      return res.send(data);
-    } else {
-      return console.error(err);
-    }
-  });
-});
-
-app.post("/image", function(req, res){
-  var newTag = new models.image(req.body);
-  newTag.save(function (err) {
-    if (err) {
-      return console.log(err);
-    }
-  });
-  return res.send(newTag);
-});
-
-app.get("/tag", function(req, res){
-  return models.tag.find("", function(err, data){
-    if (!err){ 
-      return res.send(data)
-    }
-    else{
-       return console.error(err);
-    }
-
-  }); 
-});
-
-app.get('/tag/:id', function (req, res){
-  return models.tag.findById(req.params.id, function (err, data) {
-    if (!err) {
-      return res.send(data);
-    } else {
-      return console.error(err);
-    }
-  });
-});
-
-app.post("/tag", function(req, res){
-  var newTag = new models.tag(req.body);
-  newTag.save(function (err) {
-    if (err) {
-      return console.log(err);
-    }
-  });
-  return res.send(newTag);
-});
 
 app.get("/api/images/list", api.list);
-app.get("/api/messages", api.messages);
 app.get("/api/images/tags", imagesTags.list);
+app.get("/api/tags", endpoints.tagGet);
+app.get("/api/tags/:id", endpoints.tagIdGet);
+app.get("/api/cards", endpoints.cardGet);
+app.get("/api/cards/:id", endpoints.cardIdGet);
+app.get("/api/images", endpoints.imageGet);
+app.get("/api/images/:id", endpoints.imageIdGet);
+app.get("/api/messages", endpoints.messageGet);
+app.get("/api/messages/:id", endpoints.messageIdGet);
 
+app.post("/api/messages", endpoints.messagePost);
+app.post("/api/images", endpoints.imagePost);
+app.post("/api/cards", endpoints.cardPost);
+app.post("/api/tags", endpoints.tagPost);
 app.post("/hello", helloName);
 
 app.listen(8080, function() {
--- server/Model/models.js
+++ server/Model/models.js
@@ -7,18 +7,31 @@ module.exports = function(mongoose) {
 	  data: String,
 	  tags: [tag],
 	  artist: String,
-	  age: Number
+	  age: Number,
+	  orientation: String
 	});
 	var message = mongoose.Schema({
 	  text: String,
 	  tags: [tag]
+	});
+	var card = mongoose.Schema({
+	  image: [image],
+	  messageText: String,
+	  from: String,
+	  fromEmail: String,
+	  to: String,
+	  toEmail: String,
+	  amount: Number,
+	  uuid: String,
+	  status: String
 	});
 
 
 	var models = {
 		tag : mongoose.model("tag", tag),
 		image : mongoose.model("image", image),
-		message : mongoose.model("message", message)
+		message : mongoose.model("message", message),
+		card : mongoose.model("card", card)
 	};
 
 	return models;
